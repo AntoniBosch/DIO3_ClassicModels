f325c560efe0bba00cb8ff6749d1c7bf384fe0f2
AntoniBosch <57110070+AntoniBosch@users.noreply.github.com>
2022-09-23 18:47:29 +0200

    Update DataAccess.cs

--- OrderLineViewer/DataAccess.cs
+++ OrderLineViewer/DataAccess.cs
@@ -10,7 +10,7 @@ namespace OrderLineViewer
 {
     class DataAccess
     {
-        string connectionString = "Server=localhost;Port=3306;Database=classicmodels;Uid=ClassicModelsAdmin;Pwd=tB847512963";
+        string connectionString = "Server=localhost;Port=3306;Database=classicmodels;Uid=ClassicModelsAdmin;Pwd=";
 
         public List<String> GetProductLines()
         {
